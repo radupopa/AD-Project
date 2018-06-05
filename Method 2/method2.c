0c5cd91a3edf73e1a26d28a69e86c9430eff6769
Radu Popa <radu.popa1998@gmail.com>
2018-06-05 20:12:17 +0300

    update

--- Method 2/method2.c
+++ Method 2/method2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define INF 0x3fffffff
-#define V 4
+#define V 6
 
 void find_path(int u, int v, int dist[][V], int pred[][V])
 {
@@ -58,10 +58,12 @@ void floyd(int graph[][V])
 int main(void)
 {
     int graph[V][V] = {
-            {0, 5, 10, INF},
-            {5, 0, 4, 11},
-            {10, 4, 0, 5},
-            {INF, 11, 5, 0},
+           0,      135,    INF,    87,     157,    INF,
+        135,    0,      128,    INF,    INF,    INF,
+        INF,    128,    0,      118,    44,     INF,
+        87,     INF,    118,    0,      INF,    INF,
+        157,    INF,    44,     INF,    0,      INF,
+        INF,    INF,    INF,    INF,    INF,    0
     };
     floyd(graph);
     return 0;
